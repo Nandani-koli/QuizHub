b62240ea8f7f8412f6a52b279e45a58f7d1223fe
Nandani-koli <70843941+Nandani-koli@users.noreply.github.com>
2022-10-06 19:12:23 +0530

    Update config.js

--- model/config.js
+++ model/config.js
@@ -1,6 +1,6 @@
 // mongodb+srv://nandanikoli:<password>@cluster0.tekiubm.mongodb.net/?retryWrites=true&w=majority
 const mongoose = require('mongoose');  
-const db =  "mongodb+srv://nandanikoli:0xVLXLVYYwjl220x@cluster0.tekiubm.mongodb.net/Quizhub?retryWrites=true&w=majority";
+const db =  "mongodb+srv://nandanikoli:<password>@cluster0.tekiubm.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(db).then(() => {
     console.log('connection successful');
 }).catch((err) => console.log(err));
